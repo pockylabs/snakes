bb9e1a339578f34b1e7aa2d00fb6b0f324c2c8d9
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2023-02-21 15:16:20 +0700

    tuples

---
+++ orbits/tuples.py
@@ -0,0 +1,8 @@
+# Read Only
+
+tipe_bahan_bakar = ('solar', 'pertamax')
+print(tipe_bahan_bakar)
+
+# List -> bila ingin merubah data di saat tertentu
+# Tuples -> bila tak ingin merubah data (lebih efisien dalam memory)
+
