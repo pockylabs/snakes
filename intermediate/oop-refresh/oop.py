d0e64c1001b6e3a607f52bc8886581f7b60fe4e5
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2023-03-05 01:30:58 +0700

    multiple class + adding student

--- intermediate/oop-refresh/oop.py
+++ intermediate/oop-refresh/oop.py
@@ -1,18 +1,40 @@
-class Dog:
-
-    def __init__(self, name, age):
+class Student: 
+    def __init__(self, name, age, grade):
         self.name = name
         self.age = age
+        self.grade = grade # 0 -100
 
-    def get_name(self):
-        return self.name
+    def get_grade(self):
+        return self.grade
     
-    def get_age(self):
-        return self.age
-    
-    def set_age(self, age):
-        self.age = age
 
-d = Dog("Tim", 34)
-d.set_age(23)
-print(d.get_age())
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+    
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+
+        return value / len(self.students)
+
+
+s1 = Student("Pocky", 19, 95)
+s2 = Student("Bill", 19, 75)
+s3 = Student("Jill", 19, 65)   
+
+course = Course("Science", 2)
+course.add_student(s1)
+course.add_student(s2)
+
+print(course.students[0].name)
+print(course.get_average_grade())
